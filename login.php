ee702ba0c24e2ab0f5574e37866ee780142c2edd
wassim <wassimyousfi27@gmail.com>
2023-06-07 19:04:35 +0200

    login pp

--- login.php
+++ login.php
@@ -1,9 +1,14 @@
 <?php
-
+include('connection.php');
 $email=$_POST['email'];
 $pass=$_POST['password'];
 
-echo $email;
+
+$idcom=connexion('web');
+$requete='select * from users where username= $email and password=$pass'; 
+$result=@mysqli_query($idcom,$requete);
+
+
 
 
 
