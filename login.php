82e932bb26c3eeaad24b75236092d56c7d53b446
wassim <wassimyousfi27@gmail.com>
2023-06-08 17:50:59 +0200

    login

--- login.php
+++ login.php
@@ -5,9 +5,17 @@ $pass=$_POST['password'];
 
 
 $idcom=connexion();
-$requete='select type from users where username= $email and password=$pass'; 
-$result=@mysqli_query($idcom,$requete);
-echo $result;
+$requete="select type from users where username='$email' and password='$pass'"; 
+
+
+
+
+$result=$idcom->query($requete);
+print_r($result);
+
+$result->num_rows;
+
+
 echo 'test';
 
 
