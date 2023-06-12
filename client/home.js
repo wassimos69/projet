3202168bf00fa35b0b8d2a2dd6a9b1074d5fa5e0
wassim <wassimyousfi27@gmail.com>
2023-06-12 12:52:18 +0200

    update

---
+++ client/home.js
@@ -0,0 +1,7 @@
+function myFunction(){
+var d=document.querySelector('categorie');
+if (d=="class")
+   window.location.href='www.facebook.com';
+else
+window.location.href='www.faceboceceok.com';
+}
